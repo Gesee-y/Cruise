eed7f79db3ef5e06598e9682a935242bbab6d7ce
Talom Laël <gesee37@gmail.com>
2026-01-13 22:37:18 +0100

    Added some documentation

--- test/ecs/test_table.nim
+++ test/ecs/test_table.nim
@@ -1,151 +1,180 @@
 include "../../src/ecs/table.nim"
 
 import unittest
-import tables
-
-##############################################
-# Test components
-############################################
 
 type
-  Position = object
-    x, y: int
+  Pos = object
+    x,y:float
 
-  Velocity = object
-    dx, dy: int
+  Vel = object
+    dx,dy:float
 
-############################################
-# Helpers
-############################################
+  Acc = object
+    ax,ay:float
 
-proc setupWorld(): ECSWorld =
-  var w: ECSWorld
-  new(w)
-  new(w.registry)
-  registerComponent[Position](w.registry)
-  registerComponent[Velocity](w.registry)
-  w
+proc initECSWorld(): ECSWorld =
+  var world = newECSWorld()
+  discard world.registerComponent(Pos)
+  discard world.registerComponent(Vel)
+  discard world.registerComponent(Acc)
+  world
 
-############################################
-# Tests
-############################################
+suite "Dense ECS Allocation Tests":
 
-suite "ECS Table allocation":
+  test "createPartition initializes partition only once":
+    var world = initECSWorld()
 
-  test "allocate single dense entity":
-    var w = setupWorld()
+    let arch = world.archGraph.findArchetypeFast(maskOf(0, 1))
+
+    let p1 = createPartition(world, arch)
+    let p2 = createPartition(world, arch)
+
+    check p1 == p2
+    check p1.components.len == 2
+    check p1.components.contains(0)
+    check p1.components.contains(1)
+
+
+  test "allocateEntities creates correct number of rows":
+    var world = initECSWorld()
+    let arch = maskOf(0, 1, 2)
+
+    let res = allocateEntities(world, 100, arch)
+    var total = 0
+    for (_, r) in res:
+      total += r.e - r.s
+
+    check total == 100
+
+
+  test "allocateEntities respects block boundaries":
+    var world = initECSWorld()
     let arch = maskOf(0)
 
-    let (bid, r) = w.allocateEntity(arch)
+    let n = DEFAULT_BLK_SIZE * 2 + 13
+    let res = allocateEntities(world, n, arch)
 
-    check r.e - r.s == 1
-    check bid == 0
+    var seen = 0
+    for (_, r) in res:
+      check r.e <= DEFAULT_BLK_SIZE
+      check r.s < r.e
+      seen += r.e - r.s
 
-  test "allocate multiple dense entities across blocks":
-    var w = setupWorld()
+    check seen == n
+
+
+  test "allocateEntity increments fill_index correctly":
+    var world = initECSWorld()
+    let arch = maskOf(0)
+    let node = world.archGraph.findArchetypeFast(arch)
+
+    for i in 0..<DEFAULT_BLK_SIZE:
+      discard allocateEntity(world, arch)
+
+    check node.partition.fill_index == 1
+
+
+  test "deleteRow performs swap-back correctly":
+    var world = initECSWorld()
     let arch = maskOf(0)
 
-    let res = w.allocateEntities(DEFAULT_BLK_SIZE + 10, arch)
+    let h1 = world.createEntity(arch)
+    let h2 = world.createEntity(arch)
 
-    check res.len == 2
-    check res[0][1].e == DEFAULT_BLK_SIZE
-    check res[1][1].e == 10
+    let lastId = deleteRow(world, h1.obj.id, h1.obj.archetypeId)
 
-  test "activate and deactivate dense components":
-    var w = setupWorld()
-    let arch = maskOf(0, 1)
+    check lastId == h2.obj.id or lastId == h1.obj.id
 
-    let (bid, r) = w.allocateEntity(arch)
-    let id = makeId((bid, r))
 
-    w.activateComponents(id, @[0,1])
-    w.deactivateComponents(id, @[1])
+suite "Dense ECS Migration Tests":
 
-    let pos = getvalue[Position](w.registry.entries[0])
-    let vel = getvalue[Velocity](w.registry.entries[1])
-
-    check pos.mask.len > 0
-    check vel.mask.len > 0
-
-  test "delete dense row swaps last element":
-    var w = setupWorld()
-    let arch = maskOf(0)
-
-    let (_, r1) = w.allocateEntity(arch)
-    let (_, r2) = w.allocateEntity(arch)
-
-    let id1 = r1.e-1
-    let id2 = r2.e-1
-
-    var pos = getvalue[Position](w.registry.entries[0])
-    pos[id2] = Position(x: 99, y: 99)
-
-    let last = w.deleteRow(id1, arch)
-
-    check pos[id1].x == 99
-    check last == id2.uint
-
-suite "ECS Table sparse allocation":
-
-  test "allocate sparse entity":
-    var w = setupWorld()
-
-    let id = w.allocateSparseEntity(@[0])
-
-    check w.free_list.len == 0
-    check id >= 0
-
-  test "sparse delete reuses index":
-    var w = setupWorld()
-
-    let a = w.allocateSparseEntity(@[0])
-    let b = w.allocateSparseEntity(@[0])
-
-    w.deleteSparseRow(a.uint, @[0])
-
-    let c = w.allocateSparseEntity(@[0])
-
-    check c == a
-
-  test "sparse activation sets sparse mask":
-    var w = setupWorld()
-
-    let id = w.allocateSparseEntity(@[0])
-    w.activateComponentsSparse(id, @[0])
-
-    let pos = getvalue[Position](w.registry.entries[0])
-    let bid = id div (sizeof(uint)*8)
-
-    check pos.sparse[bid].mask != 0
-
-suite "ECS Table partition changes":
-
-  test "change partition moves entity":
-    var w = setupWorld()
-
+  test "changePartition moves entity to new archetype":
+    var world = initECSWorld()
     let archA = maskOf(0)
-    let archB = maskOf(0,1)
+    let archB = maskOf(0, 1)
 
-    let (_, r) = w.allocateEntity(archA)
-    let id = r.e-1
+    let h = world.createEntity(archA)
+    let nodeB = world.archGraph.findArchetypeFast(archB)
 
-    var pos = getvalue[Position](w.registry.entries[0])
-    pos[id] = Position(x: 7, y: 8)
+    migrateEntity(world, h, nodeB)
 
-    let (last, new_id, bid) = w.changePartition(id, archA, archB)
-    let newIdx = (bid shl BLK_SHIFT) or new_id
+    check h.obj.archetypeId == nodeB.id
 
-    check pos[newIdx].x == 7
-    check last >= 0
 
-  test "partition removal shrinks correctly":
-    var w = setupWorld()
+  test "batch changePartition preserves entity count":
+    var world = initECSWorld()
+    let archA = maskOf(0)
+    let archB = maskOf(0, 1)
 
+    var ents = world.createEntities(128, archA)
+    let nodeB = world.archGraph.findArchetypeFast(archB)
+
+    migrateEntity(world, ents, nodeB)
+
+    for e in ents:
+      check e.obj.archetypeId == nodeB.id
+
+
+suite "Sparse ECS Allocation Tests":
+
+  test "allocateSparseEntity reuses free list":
+    var world = initECSWorld()
+    let comps = @[0, 1]
+
+    let a = world.allocateSparseEntity(comps)
+    deleteSparseRow(world, a, maskOf(comps))
+
+    let b = world.allocateSparseEntity(comps)
+
+    check a == b
+
+
+  test "allocateSparseEntities returns correct ranges":
+    var world = initECSWorld()
+    let comps = @[0]
+
+    let res = allocateSparseEntities(world, 100, comps)
+    var total = 0
+
+    for r in res:
+      total += r.e - r.s
+
+    check total == 100
+
+suite "Dense <-> Sparse Conversion Tests":
+
+  test "makeDense transfers all components":
+    var world = initECSWorld()
+    var s = world.createSparseEntity(0, 1, 2)
+
+    let d = world.makeDense(s)
+
+    check d.obj.archetypeId ==
+      world.archGraph.findArchetypeFast(maskOf(0,1,2)).id
+
+
+  test "makeSparse transfers all components":
+    var world = initECSWorld()
+    let d = world.createEntity(0, 1)
+
+    var s = world.makeSparse(d)
+
+    check s.mask[0] == 3'u
+
+suite "Safety and Regression Tests":
+
+  test "deleteRow on empty partition fails":
+    var world = initECSWorld()
     let arch = maskOf(0)
-    let (_, r1) = w.allocateEntity(arch)
-    let (_, r2) = w.allocateEntity(arch)
+    let node = world.archGraph.findArchetypeFast(arch)
 
-    discard w.deleteRow(r1.e-1, arch)
+    expect AssertionError:
+      discard deleteRow(world, 0, node.id)
 
-    let p = w.archetypes[arch]
-    check p.zones[p.fill_index].r.e == 1
+
+  test "changePartition rejects invalid archetype":
+    var world = initECSWorld()
+    let h = world.createEntity(maskOf(0))
+
+    expect AssertionError:
+      discard changePartition(world, h.obj.id, 9999, nil)
