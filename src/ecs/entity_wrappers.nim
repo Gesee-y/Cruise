ef84775a10129e687884fc2c20d30ac14c532189
Talom Lael <gesee37@gmail.com>
2026-05-28 12:38:39 +0100

    Documented wrappers

--- src/ecs/entity_wrappers.nim
+++ src/ecs/entity_wrappers.nim
@@ -1,52 +1,61 @@
-####################################################################################################################################################
-############################################################### ENTITY WRAPPERS ####################################################################
-####################################################################################################################################################
+# ################################################################################################################################################## #
+# ############################################################## ENTITY WRAPPERS ################################################################### #
+# ################################################################################################################################################## #
 
 type
   DWEntity* = object
+    ## A dense handle that can be used without passing the world around
     handle*:DenseHandle
     w*:ECSWorld
 
   SWEntity* = object
+    ## A sparse handle that can be used without passing the world around
     handle*:SparseHandle
     w*:ECSWorld
 
 proc link*(w: ECSWorld, d:DenseHandle): DWEntity =
+  ## Transform a dense handle into a `DWEntity` that can be use for ops without passing the world around
   DWEntity(handle:d, w:w)
 
 proc link*(w: ECSWorld, d:SparseHandle): SWEntity =
+  ## Transform a sparse handle into a `SWEntity` that can be use for ops without passing the world around
   SWEntity(handle:d, w:w)
 
-####################################################################################################################################################
-################################################################### ACCESSORS #####################################################################
-####################################################################################################################################################
+# ################################################################################################################################################## #
+# ################################################################### ACCESSORS #################################################################### #
+# ################################################################################################################################################## #
 
-## Retrieves component data from a `FragmentArray` using a `DWEntity`.
-template `[]`*[N,P,T,S,B](f: FragmentArray[N,P,T,S,B], dw:DWEntity):untyped = f[dw.handle]
+template `[]`*[N,P,T,S,B](f: FragmentArray[N,P,T,S,B], dw:DWEntity):untyped = 
+  ## Retrieves component data from a `FragmentArray` using a `DWEntity`.
+  f[dw.handle]
 
-## Retrieves component data from a `FragmentArray` using a `SWEntity`.
-template `[]`*[N,P,T,S,B](f: FragmentArray[N,P,T,S,B], sw:SWEntity):untyped = f[sw.handle]
+template `[]`*[N,P,T,S,B](f: FragmentArray[N,P,T,S,B], sw:SWEntity):untyped = 
+  ## Retrieves component data from a `FragmentArray` using a `SWEntity`.
+  f[sw.handle]
 
-## Sets component data in a `FragmentArray` for a `DWEntity`.
 template `[]=`*[N,P,T,S,B](f:var FragmentArray[N,P,T,S,B], dw:DWEntity, v:B) = 
+  ## Sets component data in a `FragmentArray` for a `DWEntity`.
   f[dw.handle] = v
 
-## Sets component data in a `FragmentArray` for a `SWEntity`.
 template `[]=`*[N,P,T,S,B](f: var FragmentArray[N,P,T,S,B], sw:SWEntity, v:B) = 
+  ## Sets component data in a `FragmentArray` for a `SWEntity`.
   f[sw.handle] = v
 
-####################################################################################################################################################
-################################################################# OPERATORS #######################################################################
-####################################################################################################################################################
+# ################################################################################################################################################## #
+# ################################################################# OPERATORS ###################################################################### #
+# ################################################################################################################################################## #
 
-## Equality operator for `DWEntity`.
-template `==`*(d1,d2:DWEntity):bool = (d1.handle == d2.handle)
+template `==`*(d1,d2:DWEntity):bool = 
+  ## Equality operator for `DWEntity`.
+  (d1.handle == d2.handle)
 
-## Equality operator for `SWEntity`.
-template `==`*(d1,d2:SWEntity):bool = (d1.handle == d2.handle)
+template `==`*(d1,d2:SWEntity):bool = 
+  ## Equality operator for `SWEntity`.
+  (d1.handle == d2.handle)
 
-## String representation operator for `DWEntity`.
-proc `$`*(dw:DWEntity):string = "dw:" & $dw.handle.obj.id & " g:" & $dw.handle.gen
+proc `$`*(dw:DWEntity):string = 
+  ## String representation operator for `DWEntity`.
+  "dw:" & $dw.handle.obj.id & " g:" & $dw.handle.gen
 
 ## String representation operator for `SWEntity`.
 proc `$`*(sw:SWEntity):string = "sw:" & $sw.handle.id & " g:" & $sw.handle.gen
