9a809d20aa171d4ee9937a195f0683cdc25b13e3
Talom Lael <gesee37@gmail.com>
2026-05-29 09:21:37 +0100

    added tests for plugins

--- test/plugins/plugins.nim
+++ test/plugins/plugins.nim
@@ -193,3 +193,105 @@ suite "Plugin system core":
 
     check p1.idtonode.len == 3
     check p1.dirty == true
+
+type
+  ResA = ref object of RootObj
+    val: int
+  ResB = ref object of RootObj
+    val: int
+
+type
+  SysA = ref object of PluginNode
+  SysB = ref object of PluginNode
+  SysC = ref object of PluginNode
+
+method asKey(p: SysA): string = "SysA"
+method asKey(p: SysB): string = "SysB"
+method asKey(p: SysC): string = "SysC"
+
+proc freshPlugin(): Plugin =
+  result = Plugin()
+  result.graph = newGraph(0)
+
+suite "mergePlugin":
+  test "merge p2 resources and remap ids":
+    var p1 = freshPlugin()
+    var p2 = freshPlugin()
+
+    let a1 = addSystem(p1, SysA())          # id 0 dans p1
+    let b2 = addSystem(p2, SysB())          # id 0 dans p2
+    let resId = p2.res_manager.addResource(ResA(val: 5))
+    p2.res_manager.addReadRequest(sys = b2, id = resId)
+
+    mergePlugin(p1, p2)
+
+    check p1.res_manager.toId.hasKey($ResA)
+
+suite "computeParallelLevel":
+
+  test "independent nodes assigned at the same level":
+    var p = freshPlugin()
+    discard addSystem(p, SysA())
+    discard addSystem(p, SysB())
+
+    computeParallelLevel(p)
+
+    check p.parallel_cache.len >= 1
+    var total = 0
+    for level in p.parallel_cache:
+      total += level[0].len + level[1].len
+    check total == 2
+
+  test "sequencial nodes assigned differents levels":
+    var p = freshPlugin()
+    let a = addSystem(p, SysA())
+    let b = addSystem(p, SysB())
+    discard addDependency(p, a, b)
+
+    computeParallelLevel(p)
+
+    var levelA = -1
+    var levelB = -1
+    for i, level in p.parallel_cache:
+      for id in level[0]:
+        if id == a: levelA = i
+        if id == b: levelB = i
+      for id in level[1]:
+        if id == a: levelA = i
+        if id == b: levelB = i
+    check levelA >= 0
+    check levelB >= 0
+    check levelA < levelB
+
+  test "dirty resets after calculation":
+    var p = freshPlugin()
+    discard addSystem(p, SysA())
+    p.dirty = true
+
+    computeParallelLevel(p)
+    check not p.dirty
+
+  test "A→B→C build 3 distinct layers":
+    var p = freshPlugin()
+    let a = addSystem(p, SysA())
+    let b = addSystem(p, SysB())
+    let c = addSystem(p, SysC())
+    discard addDependency(p, a, b)
+    discard addDependency(p, b, c)
+
+    computeParallelLevel(p)
+    check p.parallel_cache.len >= 3
+
+  test "Mainthread nodes separated from other nodes":
+    var p = freshPlugin()
+    let a = addSystem(p, SysA())
+    let b = addSystem(p, SysB())
+    p.idtonode[b].mainthread = true
+
+    computeParallelLevel(p)
+
+    var inMain = false
+    for level in p.parallel_cache:
+      for id in level[1]:
+        if id == b: inMain = true
+    check inMain
